6dfd3b11823fb4dae6ebd8f64c3f44a483d1c559
Andrew Zhukov <voland62@gmail.comgit config --global user.email voland62@gmail.comgit config --global user.email voland62@gmail.com>
2021-04-08 19:42:04 +0300

    fist commit

---
+++ t.py
@@ -0,0 +1,10 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# x = np.linspace(0, 20, 1000)  # Create a list of evenly-spaced numbers over the range
+# plt.plot(x, np.sin(x))       # Plot the sine of each x point
+# plt.show()                   # Display the plot
+
+
+s = "cool"
+print(s)
